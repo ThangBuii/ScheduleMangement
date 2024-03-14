bf2aa1b8facc11465f7e235f72d43cc19d3e30d0
ThangBuii <thangbdhe170642@fpt.edu.vn>
2024-03-14 07:54:01 +0700

    Add data service

--- PRN221_ASSIGNMENT/Service/DataService.cs
+++ PRN221_ASSIGNMENT/Service/DataService.cs
@@ -7,12 +7,15 @@ namespace PRN221_ASSIGNMENT.Service
     public class DataService
     {
         private ScheduleManagementContext _context;
+
+        private SlotService slotService;
         public DataService(ScheduleManagementContext context)
         {
             _context = context;
+            slotService = new SlotService(_context);
         }
 
-        private ScheduleDTO GetFullData(CsvData data)
+        private ScheduleDTO GetInitialData(CsvData data)
         {
             ScheduleDTO scheduleDTO = new ScheduleDTO();
 
@@ -30,22 +33,80 @@ namespace PRN221_ASSIGNMENT.Service
             
             Schedule1.ClassId = _context.Classes.FirstOrDefault(c => c.Code == data.Class).Id;
             Schedule2.ClassId = Schedule1.ClassId;
+
+            if (data.TimeSlot[0] == 'A')
+            {
+                Schedule1.SlotId = 1;
+                Schedule2.SlotId = 2;
+            }
+            else if (data.TimeSlot[0] == 'P')
+            {
+                Schedule1.SlotId = 3;
+                Schedule2.SlotId = 4;
+            }
+
+            Schedule1.Date = slotService.FindInitialDate(data.TimeSlot[1]);
+            Schedule2.Date = slotService.FindInitialDate(data.TimeSlot[2]);
             
-            Schedule1.SlotId = 0;
-            Schedule1.Date = DateTime.Now;
             return scheduleDTO;
-            
+        }
+
+        public List<Schedule> GetListSchedule(ScheduleDTO scheduleDTO)
+        {
+            List<Schedule> schedules = new List<Schedule>();
+            Schedule schedule1 = scheduleDTO.Schedule1;
+            schedules.Add(schedule1);
+            Schedule schedule2 = scheduleDTO.Schedule2;
+            schedules.Add(schedule2);
+            for (int i = 1; i <= 20; i++)
+            {
+                Schedule newSchedule1 = schedule1;
+                newSchedule1.Date = slotService.FindNextDate(i, schedule1.Date);
+                schedules.Add(newSchedule1);
+                Schedule newSchedule2 = schedule2;
+                newSchedule2.Date = slotService.FindNextDate(i, schedule2.Date);
+                schedules.Add(newSchedule1);
+            }
+
+            return schedules;
+        }
+
+        public bool IsExist(CsvData data)
+        {
+            if (_context.Teachers.FirstOrDefault(t => t.Code == data.Teacher) == null) return false;
+            if (_context.Subjects.FirstOrDefault(t => t.Code == data.Subject) == null) return false;
+            if (_context.Classes.FirstOrDefault(t => t.Code == data.Class) == null) return false;
+            if (_context.Rooms.FirstOrDefault(t => t.Code == data.Room) == null) return false;
+            if(slotService.CheckExists(data.TimeSlot)) return false;
+
+            return true;
         }
 
         public bool ValidateData(CsvData data)
         {
+            if (IsExist(data))
+            {
 
+            }
             return false;
+        }
+
+        public void SaveToDb(List<Schedule> schedules) 
+        {
+            foreach(Schedule s in schedules)
+            {
+                _context.Add(s);
+            }
+            _context.SaveChanges();
         }
 
         public void AddDataToDatabase(CsvData data)
         {
-            
+            ScheduleDTO scheduleDTO = GetInitialData(data);
+            List<Schedule> schedules = GetListSchedule(scheduleDTO);
+
+
+            SaveToDb(schedules);
         }
     }
 }
--- PRN221_ASSIGNMENT/Service/SlotService.cs
+++ PRN221_ASSIGNMENT/Service/SlotService.cs
@@ -6,6 +6,7 @@ namespace PRN221_ASSIGNMENT.Service
     public class SlotService
     {
         private string[] validSlots = { "A24", "A35", "A46", "A52", "A63", "P23", "P35", "P46", "P53", "P63" };
+        private static DateTime InitialDate = new DateTime(2024, 04, 28);
         private ScheduleManagementContext _context;
         public SlotService(ScheduleManagementContext context)
         {
@@ -17,10 +18,21 @@ namespace PRN221_ASSIGNMENT.Service
             return Array.IndexOf(validSlots, slotName) != -1;
         }
 
-        //public SlotDTO FindSlot(string slotName)
-        //{
-        //    SlotDTO slot = new SlotDTO();
-           
-        //}
+        public DateTime FindInitialDate(char date)
+        {
+            int dayOfWeek = int.Parse(date.ToString());
+            int daysToAdd = dayOfWeek - 2;
+
+            daysToAdd = (daysToAdd + 7) % 7;
+
+            return InitialDate.AddDays(daysToAdd);
+        }
+
+        public DateTime FindNextDate(int weekToSkip, DateTime date)
+        {
+            int dayToSkip = weekToSkip * 7;
+
+            return date.AddDays(dayToSkip);
+        }
     }
 }
